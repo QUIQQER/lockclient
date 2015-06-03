804534e127c1132b9a09f32c637c81bbe538e626
Henning Leutz <leutz@pcsg.de>
2015-06-03 13:23:40 +0200

    Client

---
+++ src/QUI/Lockserver/Client.php
@@ -0,0 +1,202 @@
+<?php
+
+/**
+ * This file contains \QUI\Lockserver\Client
+ */
+
+namespace QUI\Lockserver;
+
+use QUI;
+
+/**
+ * Class Client - for Lockserver v2
+ *
+ * @author www.pcsg.de (Henning Leutz)
+ * @package QUI\Lockserver
+ */
+class Client extends QUI\QDOM
+{
+    /**
+     * Constructor
+     *
+     * @param array $params
+     */
+    public function __construct($params = array())
+    {
+        $this->setAttributes(array(
+            'lockServer'       => 'https://lock.quiqqer.com',
+            'composerJsonFile' => '',
+            'composerLockFile' => ''
+        ));
+
+        $this->setAttributes($params);
+    }
+
+    /**
+     * Execute the update command at the lock server and return the composer.lock file
+     *
+     * @param array|string $packages
+     *
+     * @return string
+     * @throws QUI\Exception
+     */
+    public function update($packages)
+    {
+        if (is_string($packages)) {
+            $packages = array($packages);
+        }
+
+        return $this->_send('/v2/require', array(
+            'package' => json_encode($packages)
+        ));
+    }
+
+    /**
+     * Execute the update command with the --dry-run flag
+     * With dryUpdate() you can check if updates exist.
+     *
+     * @param array|string $packages - name of the package
+     *
+     * @return Array
+     * @throws QUI\Exception
+     */
+    public function dryUpdate($packages = '')
+    {
+        if (is_string($packages)) {
+            $packages = array($packages);
+        }
+
+        if (empty($packages)) {
+            $packages = '';
+        }
+
+        return $this->_send('/v2/require/dry', array(
+            'package' => json_encode($packages)
+        ));
+    }
+
+    /**
+     * Execute the require command at the lock server and return the composer.lock file
+     *
+     * @param string $package - name of the package
+     * @param string $version - (optional), version of the package
+     *
+     * @return string
+     * @throws QUI\Exception
+     */
+    public function requires($package, $version = '')
+    {
+        return $this->_send('/v2/require', array(
+            'package' => $package,
+            'version' => $version
+        ));
+    }
+
+    /**
+     * Execute the require command with the --dry-run flag
+     *
+     * @param string $package - name of the package
+     * @param string $version - (optional), version of the package
+     *
+     * @return string
+     * @throws QUI\Exception
+     */
+    public function dryRequires($package, $version = '')
+    {
+        return $this->_send('/v2/require/dry', array(
+            'package' => $package,
+            'version' => $version
+        ));
+    }
+
+    /**
+     * Validate the JSON data
+     *
+     * @param string $json - json data
+     *
+     * @throws QUI\Exception
+     */
+    protected function _checkJSON($json)
+    {
+        $data = json_decode($json);
+
+        if (!$data) {
+
+            throw new QUI\Exception(
+                QUI::getLocale()->get(
+                    'quiqqer/quiqqer',
+                    'exception.lock.client.composerJson.invalid'
+                ),
+                400
+            );
+        }
+    }
+
+    /**
+     * Send a requrest to the lockserver
+     *
+     * @param string $url
+     * @param array  $postFields
+     *
+     * @return string
+     *
+     * @throws QUI\Exception
+     */
+    protected function _send($url, array $postFields)
+    {
+        $lockServer = $this->getAttribute('lockServer');
+        $lockFile = $this->getAttribute('composerJsonFile');
+        $jsonFile = $this->getAttribute('composerLockFile');
+
+        if (empty($lockServer)) {
+
+            throw new QUI\Exception(
+                QUI::getLocale()->get(
+                    'quiqqer/quiqqer',
+                    'exception.lock.client.unknown.lock.server'
+                ),
+                400
+            );
+        }
+
+        // lock json
+        if (empty($lockFile) || !file_exists($lockFile)) {
+            throw new QUI\Exception(
+                QUI::getLocale()->get(
+                    'quiqqer/quiqqer',
+                    'exception.lock.client.lockfile.not.found'
+                ),
+                404
+            );
+        }
+
+        // composer json
+        if (empty($jsonFile) || !file_exists($jsonFile)) {
+            throw new QUI\Exception(
+                QUI::getLocale()->get(
+                    'quiqqer/quiqqer',
+                    'exception.lock.client.composerjson.not.found'
+                ),
+                404
+            );
+        }
+
+        $postFields['composerJson'] = file_get_contents($jsonFile);
+        $postFields['composerLock'] = file_get_contents($lockFile);
+
+
+        $Curl = QUI\Utils\Request\Url::Curl($lockServer.$url, array(
+            CURLOPT_POST       => 1,
+            CURLOPT_POSTFIELDS => http_build_query($postFields)
+        ));
+
+        $result = QUI\Utils\Request\Url::exec($Curl);
+        $info = curl_getinfo($Curl);
+
+        if ($info['http_code'] == 200) {
+            return $result;
+        }
+
+        throw new QUI\Exception($result, 400);
+    }
+}
