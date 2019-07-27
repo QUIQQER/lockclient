6fbebb52666ac7481071b3f8d88b9051a1bfa4a5
Florian Bogner <f.bogner@pcsg.de>
2019-07-27 11:40:13 +0200

    feat: Avoids extending the QUI\Exception class if the Lockclient gets used outside of a running QUIQQER installation e.g. the QSetup. quiqqer/qsetup#138

--- src/QUI/Lockclient/Exceptions/LockServerException.php
+++ src/QUI/Lockclient/Exceptions/LockServerException.php
@@ -2,9 +2,29 @@
 
 namespace QUI\Lockclient\Exceptions;
 
-use QUI\Exception;
+/*
+ *  As the lock client must be usable as standalone package for the setup, we must include a failsafe check
+ *  to avoid using classes that are not present during the setup.
+ *  This will check if the QUI and the QUI\Exception classes exist before extending them
+ */
 
-class LockServerException extends Exception
-{
-    
+use Throwable;
+
+if (class_exists('QUI') && class_exists('QUI\Exception')) {
+    class LockServerException extends QUI\Exception
+    {
+    }
+} else {
+    class LockServerException extends \Exception
+    {
+        public function __construct($message = "", $code = 0, Throwable $previous = null)
+        {
+            // The QUI\Exception gets called with an array as message, which gets used to get a translated error message.
+            // The array contains the package in the first field and the locale variable name in the second.
+            if (is_array($message) && isset($message[1])) {
+                $message = $message[1];
+            }
+            parent::__construct($message, $code, $previous);
+        }
+    }
 }
